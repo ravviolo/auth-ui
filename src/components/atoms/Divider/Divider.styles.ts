f5912c0dd3bcbf61db15614db62611a4a0c827a6
Jolanta Ślipko <j.slipko21@gmail.com>
2022-08-24 17:53:50 +0200

    Fix spacing

--- src/components/atoms/Divider/Divider.styles.ts
+++ src/components/atoms/Divider/Divider.styles.ts
@@ -4,8 +4,13 @@ import { colors } from 'theme';
 
 export const Container = styled.div`
   position: relative;
+  height: 10px;
 `;
 export const Line = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 100%;
   height: 1px;
   background-color: ${colors.textLight};

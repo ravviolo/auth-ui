417f4be7a82aae5d91b7ada71ab9a4ebf33fec32
Jolanta Ślipko <j.slipko21@gmail.com>
2022-08-24 19:18:37 +0200

    Remove unused code, add types

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,18 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const colors = {
+interface Colors {
+  text: string;
+  textLight: string;
+  textWhite: string;
+  primary: string;
+  primaryDark: string;
+  background: string;
+}
+
+export const colors: Colors = {
   text: '#3D3D3D',
   textLight: '#9E9E9E',
   textWhite: '#EDEDED',
   primary: '#FF4684',
   primaryDark: '#F9256C',
   background: '#5F9CC8',
-};
-
-export const fontWeight = {
-  thin: 300,
-  regular: 400,
-  bolder: 500,
 };
 
 export const GlobalStyle = createGlobalStyle`

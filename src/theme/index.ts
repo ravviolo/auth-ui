18b88e3d31a7ce3cbe5f2a388abc3c41a33d7a33
Jolanta Ślipko <j.slipko21@gmail.com>
2022-08-24 16:21:01 +0200

    Move text color to global styles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -21,6 +21,7 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+    color: ${colors.text}
 }
 html {
     height: 100%;

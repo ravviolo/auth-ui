f839943b7afb0fe8901f214907d59d3f1121a646
Jolanta Ślipko <j.slipko21@gmail.com>
2022-08-24 18:50:36 +0200

    Add responsivness to main container

--- src/components/templates/LoginTemplate/LoginTemplate.styles.ts
+++ src/components/templates/LoginTemplate/LoginTemplate.styles.ts
@@ -3,12 +3,33 @@ import styled from 'styled-components';
 import { Flex } from 'components/atoms';
 
 export const Container = styled.main`
-  width: 25vw;
-  height: 70vh;
+  height: 560px;
   padding: 3em;
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 2px 2px 8px 1px rgba(0, 0, 0, 0.3);
+
+  @media screen and (min-width: 1800px) {
+    width: 20vw;
+  }
+  @media screen and (max-width: 1800px) {
+    width: 20vw;
+  }
+  @media screen and (max-width: 1600px) {
+    width: 25vw;
+  }
+  @media screen and (max-width: 1100px) {
+    width: 40vw;
+  }
+  @media screen and (max-width: 700px) {
+    width: 50vw;
+  }
+  @media screen and (max-width: 570px) {
+    width: 70vw;
+  }
+  @media screen and (max-width: 430px) {
+    width: 80vw;
+  }
 `;
 
 export const TemplateFlex = styled(Flex)`

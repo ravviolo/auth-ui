7593d0539613442775ecf162f35789acf9896857
Jolanta Ślipko <j.slipko21@gmail.com>
2022-08-30 14:29:50 +0200

    Refactor Heading stories

--- src/components/atoms/Heading/Heading.stories.tsx
+++ src/components/atoms/Heading/Heading.stories.tsx
@@ -5,13 +5,12 @@ import { Heading } from 'components/atoms';
 export default {
   title: 'Atoms/Heading',
   component: Heading,
+  args: {
+    title: 'This is an h1 heading',
+  },
 } as ComponentMeta<typeof Heading>;
 
-const Template: ComponentStory<typeof Heading> = (args) => (
+export const Default: ComponentStory<typeof Heading> = (args) => (
   <Heading {...args} />
 );
-
-export const Default = Template.bind({});
-Default.args = {
-  title: 'I am a heading',
-};
+Default.storyName = 'Heading';

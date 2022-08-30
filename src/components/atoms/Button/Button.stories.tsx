b47260d2fead4c7b1c098401b539ebf00ce346dc
Jolanta Ślipko <j.slipko21@gmail.com>
2022-08-30 14:27:07 +0200

    Refactor Button stories

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -5,32 +5,13 @@ import { Button } from 'components/atoms';
 export default {
   title: 'Atoms/Button',
   component: Button,
+  args: {
+    type: 'button',
+    label: 'Button',
+  },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => (
-  <div style={{ width: '10rem' }}>
-    <Button {...args} />
-  </div>
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  type: 'button',
-  label: 'Button',
-};
-
-export const Submit = Template.bind({});
-Submit.args = {
-  type: 'submit',
-  label: 'Submit',
-};
-Submit.argTypes = {
-  onClick: { action: 'onSubmit' },
-};
-
-export const FullWidth: ComponentStory<typeof Button> = (args) => (
+export const Default: ComponentStory<typeof Button> = (args) => (
   <Button {...args} />
 );
-FullWidth.args = {
-  ...Default.args,
-};
+Default.storyName = 'Button';

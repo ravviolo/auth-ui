d846543fe0efad1c0d90ab0f9b7800ea421ea25c
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-02 11:23:20 +0200

    Sort props

--- src/components/molecules/AuthHeader/AuthHeader.tsx
+++ src/components/molecules/AuthHeader/AuthHeader.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const AuthHeader = ({ title }: Props) => {
   return (
     <header>
-      <Heading title={title} testId="auth-heading" />
+      <Heading testId="auth-heading" title={title} />
     </header>
   );
 };

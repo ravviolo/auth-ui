96c8c5908d5ad287f42bbe3dea36581169672758
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-02 12:48:59 +0200

    Add InputField tests

---
+++ src/components/atoms/InputField/InputField.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { create } from 'react-test-renderer';
+
+import { InputField } from 'components/atoms';
+
+import { Props as InputFieldProps } from './InputField';
+
+describe('InputField', () => {
+  const props: InputFieldProps = {
+    id: 'test-input-field',
+    name: 'test-input-field',
+    testId: 'test-input-field',
+    type: 'text',
+  };
+
+  it('renders initially as empty input', () => {
+    render(<InputField {...props} />);
+
+    const inputField = screen.getByTestId('test-input-field');
+
+    expect(inputField).toHaveValue('');
+  });
+
+  it('displays entered text', async () => {
+    render(<InputField {...props} />);
+
+    const inputField = screen.getByTestId('test-input-field');
+
+    await userEvent.type(inputField, 'Testing input field');
+
+    expect(inputField).toHaveValue('Testing input field');
+  });
+
+  it('displays part of entered text after deleting some characters', async () => {
+    render(<InputField {...props} />);
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    const inputField = screen.getByTestId(
+      'test-input-field'
+    ) as HTMLInputElement;
+
+    await userEvent.type(inputField, 'Testing input field');
+    inputField.setSelectionRange(7, 19);
+    await userEvent.keyboard('{backspace}');
+
+    expect(inputField).toHaveValue('Testing');
+  });
+
+  it('matches snapshot', () => {
+    const tree = create(<InputField {...props} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});

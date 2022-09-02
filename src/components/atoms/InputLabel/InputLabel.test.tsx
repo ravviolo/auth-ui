e4e5dd94544fcb5eb8771be914112f6a8d2ed406
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-02 12:55:55 +0200

    Add InputLabel tests

---
+++ src/components/atoms/InputLabel/InputLabel.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { create } from 'react-test-renderer';
+
+import { InputLabel } from 'components/atoms';
+
+import { Props as InputLabelProps } from './InputLabel';
+
+describe('InputLabel', () => {
+  const props: InputLabelProps = {
+    htmlFor: 'test-input-label',
+    label: 'Test Input',
+    testId: 'test-input-label',
+  };
+
+  it('displays label text received via props', () => {
+    render(<InputLabel {...props} />);
+
+    const inputLabel = screen.getByTestId('test-input-label');
+
+    expect(inputLabel).toHaveTextContent('Test Input');
+  });
+
+  it('matches snapshot', () => {
+    const tree = create(<InputLabel {...props} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});

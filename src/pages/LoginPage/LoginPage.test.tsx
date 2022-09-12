d570527c482ecde1a97aabad4ec57e39b9584ca9
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-12 13:06:06 +0200

    Update page tests

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,12 +1,13 @@
 import { screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import * as reduxHooks from 'app/hooks';
-import * as userSlice from 'features/user/userSlice';
+import { RootState } from 'app/store';
 import { LoginPage } from 'pages';
 import { createWithProviders, renderWithProviders } from 'utils/test-utils';
 
 describe('LoginPage', () => {
+  const testEmail = 'test@test.com';
+
   it("initially should have no user's data and hold user status as 'idle'", () => {
     const { store } = renderWithProviders(<LoginPage />);
 
@@ -14,10 +15,6 @@ describe('LoginPage', () => {
   });
 
   it("after log in should save user's data, change user's status to 'complete'", async () => {
-    const mockLoginUser = jest.spyOn(userSlice, 'loginUser');
-    const mockDispatch = jest.spyOn(reduxHooks, 'useAppDispatch');
-    const testEmail = 'test@test.com';
-
     const { store } = renderWithProviders(<LoginPage />);
 
     const template = screen.getByTestId('login-template-test-id');
@@ -33,12 +30,48 @@ describe('LoginPage', () => {
     await userEvent.type(passwordInputField, 'password');
     await userEvent.click(loginBtn);
 
-    expect(mockLoginUser).toHaveBeenCalledTimes(1);
-    expect(mockDispatch).toBeCalledTimes(2);
-
     const userStore = store.getState().user;
 
     expect(userStore.status).toBe('complete');
+    expect(userStore.userData?.firstname).toBe('Anakin');
+    expect(userStore.userData?.lastname).toBe('Skywalker');
+    expect(userStore.userData?.email).toBe(testEmail);
+  });
+
+  it("if user exist should update store with new user's data", async () => {
+    const preloadedState: RootState = {
+      user: {
+        status: 'complete',
+        userData: {
+          avatarUrl: 'url',
+          birthday: 'birthday',
+          createdAt: 'createdAt',
+          email: 'email',
+          firstname: 'firstname',
+          lastname: 'lastname',
+          username: 'username',
+          uuid: 'uuid',
+        },
+      },
+    };
+
+    const { store } = renderWithProviders(<LoginPage />, { preloadedState });
+
+    const template = screen.getByTestId('login-template-test-id');
+    const emailInputField = within(template).getByTestId(
+      'input-field-email-test-id'
+    );
+    const passwordInputField = within(template).getByTestId(
+      'input-field-password-test-id'
+    );
+    const loginBtn = within(template).getByTestId('btn-submit-login-test-id');
+
+    await userEvent.type(emailInputField, testEmail);
+    await userEvent.type(passwordInputField, 'password');
+    await userEvent.click(loginBtn);
+
+    const userStore = store.getState().user;
+
     expect(userStore.userData?.firstname).toBe('Anakin');
     expect(userStore.userData?.lastname).toBe('Skywalker');
     expect(userStore.userData?.email).toBe(testEmail);
--- src/pages/SignUpPage/SignUpPage.test.tsx
+++ src/pages/SignUpPage/SignUpPage.test.tsx
@@ -1,12 +1,13 @@
 import { screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import * as reduxHooks from 'app/hooks';
-import * as userSlice from 'features/user/userSlice';
+import { RootState } from 'app/store';
 import { SignUpPage } from 'pages';
 import { createWithProviders, renderWithProviders } from 'utils/test-utils';
 
 describe('SignUpPage', () => {
+  const testEmail = 'test@test.com';
+
   it("initially should have no user's data and hold user status as 'idle'", () => {
     const { store } = renderWithProviders(<SignUpPage />);
 
@@ -14,10 +15,6 @@ describe('SignUpPage', () => {
   });
 
   it("after sign up should save user's data, change user's status to 'complete'", async () => {
-    const mockRegisterUser = jest.spyOn(userSlice, 'registerUser');
-    const mockDispatch = jest.spyOn(reduxHooks, 'useAppDispatch');
-    const testEmail = 'test@test.com';
-
     const { store } = renderWithProviders(<SignUpPage />);
 
     const template = screen.getByTestId('signup-template-test-id');
@@ -33,12 +30,48 @@ describe('SignUpPage', () => {
     await userEvent.type(passwordInputField, 'password');
     await userEvent.click(signUpBtn);
 
-    expect(mockRegisterUser).toHaveBeenCalledTimes(1);
-    expect(mockDispatch).toBeCalledTimes(2);
-
     const userStore = store.getState().user;
 
     expect(userStore.status).toBe('complete');
+    expect(userStore.userData?.firstname).toBe('Baby');
+    expect(userStore.userData?.lastname).toBe('Yoda');
+    expect(userStore.userData?.email).toBe(testEmail);
+  });
+
+  it("if user exist should update store with new user's data", async () => {
+    const preloadedState: RootState = {
+      user: {
+        status: 'complete',
+        userData: {
+          avatarUrl: 'url',
+          birthday: 'birthday',
+          createdAt: 'createdAt',
+          email: 'email',
+          firstname: 'firstname',
+          lastname: 'lastname',
+          username: 'username',
+          uuid: 'uuid',
+        },
+      },
+    };
+
+    const { store } = renderWithProviders(<SignUpPage />, { preloadedState });
+
+    const template = screen.getByTestId('signup-template-test-id');
+    const emailInputField = within(template).getByTestId(
+      'input-field-email-test-id'
+    );
+    const passwordInputField = within(template).getByTestId(
+      'input-field-password-test-id'
+    );
+    const signUpBtn = within(template).getByTestId('btn-submit-signup-test-id');
+
+    await userEvent.type(emailInputField, testEmail);
+    await userEvent.type(passwordInputField, 'password');
+    await userEvent.click(signUpBtn);
+
+    const userStore = store.getState().user;
+
     expect(userStore.userData?.firstname).toBe('Baby');
     expect(userStore.userData?.lastname).toBe('Yoda');
     expect(userStore.userData?.email).toBe(testEmail);
